6ace8151f3f2d4d899baa7c1166b33afb4b4634c
Gautam-Sahil <193165968+Gautam-Sahil@users.noreply.github.com>
2025-11-04 23:58:27 +0530

    here we again 4

--- server/model/AuditLog.js
+++ server/model/AuditLog.js
@@ -4,7 +4,11 @@ const auditLogSchema = new mongoose.Schema(
   {
     eventName: { type: String, required: true },  // e.g. clerk/user.created
     functionName: { type: String, required: true }, // e.g. sync-user-from-clerk
-    userId: { type: String },
+    userId: { type: String,  required: true },
+      _id: {type: String, required: true},
+      name: {type: String, required: true},
+    email: {type: String, required: true},
+    image: {type: String, required: true},
     status: { type: String, enum: ["success", "error"], default: "success" },
     message: { type: String },
     data: { type: Object }, // raw event or extra info
