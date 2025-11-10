8a7b750d20e66dd7cb7b1e2caa31f370a3873c26
Gautam-Sahil <193165968+Gautam-Sahil@users.noreply.github.com>
2025-11-11 02:33:45 +0530

    here we go again 8

--- server/model/Bookings.js
+++ server/model/Bookings.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const bookingsSchema = new mongoose.Schema({
-    user: {type: String, required: true, ref: "user"},
+    user: {type: String, required: true, ref: "User"},
      show: {type: String, required: true, ref: "Show"},
      amount: {type: Number, required: true},
        bookedSeats: {type: Array, required: true},
