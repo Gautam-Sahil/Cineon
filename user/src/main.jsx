9b3e18e415172701ffa228bac43992c0a8b798a6
Gautam-Sahil <193165968+Gautam-Sahil@users.noreply.github.com>
2025-12-04 03:23:23 +0530

    almost fixed

--- user/src/main.jsx
+++ user/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
   import { ClerkProvider } from '@clerk/clerk-react'
   import { AppProvider } from './context/Appcontext.jsx'
 
-// Import your Publishable Key
+
   const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
   if (!PUBLISHABLE_KEY) {
