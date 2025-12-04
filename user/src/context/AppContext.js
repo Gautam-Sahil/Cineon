5e9fe7949958a0cb652136d03020486d2dec1857
Gautam-Sahil <193165968+Gautam-Sahil@users.noreply.github.com>
2025-12-04 05:30:25 +0530

    something fixed againnnnnnn

--- user/src/context/AppContext.js
+++ user/src/context/AppContext.js
@@ -1,5 +1,12 @@
+// AppContext.js
 import { createContext, useContext } from "react";
+import { AppProvider as Provider } from "./Appcontext.jsx";
 
+// Create the context
 export const Appcontext = createContext();
 
+// Custom hook to use context
 export const useAppContext = () => useContext(Appcontext);
+
+// Re-export AppProvider so it can be imported from AppContext.js
+export const AppProvider = Provider;
